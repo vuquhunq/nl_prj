c662fdbdbdcb494b26b9d10ebaaacf0e19f8018b
Henry <vuquhunq@gmail.com>
2022-04-09 16:30:47 +0700

    add service category

--- src/service/CategoryService.js
+++ src/service/CategoryService.js
@@ -6,5 +6,21 @@ class CategoryService {
       .then((res) => res.data)
       .catch((err) => console.log(err));
   }
+  addCategory(payload) {
+    return axiosInstance
+      .post("/category/", payload)
+      .then((res) => alert("Thành công"))
+      .catch((err) => alert(err));
+  }
+  updateCategory(payload) {
+    return axiosInstance
+      .put("/category/", payload)
+      .then((res) => console.log("Thành công"));
+  }
+  deleteCategory(id_category) {
+    return axiosInstance
+      .delete(`/category/${id_category}`)
+      .then((res) => alert("Thành công"));
+  }
 }
 export default new CategoryService();
