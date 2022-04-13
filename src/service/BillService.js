c574ee4980e0f1bf0d31812a4be970ed54473633
tnha1002 <ngocha6605@gmail.com>
2022-04-13 17:22:33 +0700

    backup

--- src/service/BillService.js
+++ src/service/BillService.js
@@ -76,6 +76,18 @@ class BillService {
       .then((res) => console.log(res.data))
       .catch((err) => alert(err));
   }
+
+  updateBillService(payload){
+    return axiosInstance
+      .put("/bill/",payload,{
+        headers: {
+          Authorization: `Bearer ${access_token}`,
+          "Content-Type": "Application/json",
+        },
+      })
+      .then((res) => console.log(res.data))
+      .catch((err) => alert(err));
+  }
 }
 
 export default new BillService();
